ea00b602375443515db5dc5deb0abc210810c212
sarlokbhat <138982863+sarlokbhat@users.noreply.github.com>
2023-07-08 18:12:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Dosa')
-streamlit.text('Idly')
-streamlit.text('Uthappam')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
